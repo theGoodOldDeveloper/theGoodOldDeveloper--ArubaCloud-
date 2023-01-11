832d9cb9518e5e96f4ed0595af864fc9ab0a78f1
Tibor Végh <121199642+theGoodOldDeveloper@users.noreply.github.com>
2023-01-11 20:12:30 +0100

    5/5 OK 😎

--- public/js/script.js
+++ public/js/script.js
@@ -11,5 +11,5 @@ document.getElementById('app-4').addEventListener('click', () => {
     window.location.href = 'http://thegoodolddeveloper.website:9876/'
 })
 document.getElementById('app-5').addEventListener('click', () => {
-    console.log('klikkeltem  5')
+    window.location.href = 'http://thegoodolddeveloper.website:9999/'
 })
