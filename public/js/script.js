ff8134941891eb7f96bf06293aa7d3fe21e2a137
theGoodOldDeveloper <theGoodOldDeveloper@gmail.com>
2023-02-28 23:30:59 +0100

    root presentation page😊

--- public/js/script.js
+++ public/js/script.js
@@ -13,3 +13,15 @@ document.getElementById('app-4').addEventListener('click', () => {
 document.getElementById('app-5').addEventListener('click', () => {
     window.location.href = 'http://thegoodolddeveloper.website:9999/'
 })
+
+
+$(".mainBTN").mouseenter(function(){
+  $(".mainBTN").css("color", "red");
+  $(".mainBTN").css("cursor", "pointer");
+  $(".mainBTN").css("font-size", "3rem");
+});
+$(".mainBTN").mouseleave(function(){
+  $(".mainBTN").css("color", "white");
+  $(".mainBTN").css("font-size", "2.5rem");
+});
+
