631545e344285bc01cede744314e22ff0999d320
theGoodOldDeveloper <theGoodOldDeveloper@gmail.com>
2024-02-18 21:05:28 +0100

    add Erci webpage 👀

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,37 @@
-document.getElementById('app-1').addEventListener('click', () => {
-    window.location.href = 'http://thegoodolddeveloper.website:7777/'
-})
-document.getElementById('app-2').addEventListener('click', () => {
-    window.location.href = 'http://thegoodolddeveloper.website:7766/'
-})
-document.getElementById('app-3').addEventListener('click', () => {
-    window.location.href = 'http://thegoodolddeveloper.website:3333/'
-})
-document.getElementById('app-4').addEventListener('click', () => {
-    window.location.href = 'http://thegoodolddeveloper.website:9876/'
-})
-document.getElementById('app-5').addEventListener('click', () => {
-    window.location.href = 'http://thegoodolddeveloper.website:9999/'
-})
-document.getElementById('app-6').addEventListener('click', () => {
-    window.location.href = 'http://thegoodolddeveloper.website:5011/'
-})
-document.getElementById('app-7').addEventListener('click', () => {
-    window.location.href = 'http://thegoodolddeveloper.com:5001/'
-})
-document.getElementById('app-8').addEventListener('click', () => {
-    window.location.href = 'https://photovegh.synology.me/'
-})
-
+document.getElementById("app-1").addEventListener("click", () => {
+  window.location.href = "http://thegoodolddeveloper.com:7777/";
+});
+document.getElementById("app-2").addEventListener("click", () => {
+  window.location.href = "http://thegoodolddeveloper.com:7766/";
+});
+document.getElementById("app-3").addEventListener("click", () => {
+  window.location.href = "http://thegoodolddeveloper.com:3333/";
+});
+document.getElementById("app-4").addEventListener("click", () => {
+  window.location.href = "http://thegoodolddeveloper.com:9876/";
+});
+document.getElementById("app-5").addEventListener("click", () => {
+  window.location.href = "http://thegoodolddeveloper.com:9999/";
+});
+document.getElementById("app-6").addEventListener("click", () => {
+  window.location.href = "http://thegoodolddeveloper.com:5011/";
+});
+document.getElementById("app-7").addEventListener("click", () => {
+  window.location.href = "http://217.13.101.72:5001/";
+});
+document.getElementById("app-8").addEventListener("click", () => {
+  window.location.href = "https://photovegh.synology.me/";
+});
+document.getElementById("app-9").addEventListener("click", () => {
+  window.location.href = "https://erikahorvath.com/";
+});
 
 $(".mainBTN").mouseenter(function () {
-    $(".mainBTN").css("color", "red");
-    $(".mainBTN").css("cursor", "pointer");
-    $(".mainBTN").css("font-size", "3rem");
+  $(".mainBTN").css("color", "red");
+  $(".mainBTN").css("cursor", "pointer");
+  $(".mainBTN").css("font-size", "3rem");
 });
 $(".mainBTN").mouseleave(function () {
-    $(".mainBTN").css("color", "white");
-    $(".mainBTN").css("font-size", "2.5rem");
+  $(".mainBTN").css("color", "white");
+  $(".mainBTN").css("font-size", "2.5rem");
 });
-
